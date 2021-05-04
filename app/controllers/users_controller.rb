2b5ec113bdedcd302d8b3c4dff6215bffbdcad32
NickyEXE <dovern42@gmail.com>
2021-05-04 16:07:33 -0400

    add UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  get "/users/new" do
+    @user = User.new
+    erb :"users/new"
+  end
+
+  post "/users" do
+    @user = User.create(params)
+    if @user.valid?
+      redirect_to "users/#{user.id}"
+    else
+      erb :"users/new"
+    end
+  end
+
+  get "/users/:id" do
+    @user = User.find(params[:id])
+    erb :"users/show"
+  end
+
+end
